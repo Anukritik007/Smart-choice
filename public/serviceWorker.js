d790869b9ed8cd3ff23d125e993f813730498ee1
Anukritik007 <anukritik77@gmail.com>
2021-06-23 10:25:23 +0530

    make PWA

---
+++ public/serviceWorker.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-restricted-globals */
+const CACHE_NAME = "smart-choice-cache-v1";
+const urlsToCache = ["/", "/index.html"];
+
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+  // In order to activate the new service worker, you have to close all pages, which are controlled by the old service worker.
+  // If we want to avoid that, we can skip the waiting phase by adding self.skipWaiting()
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", function (event) {
+  // inform what to do with cached files
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
